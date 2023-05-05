3672e09b2732705024e6847db1e693947200f12a
eschindev <118208710+eschindev@users.noreply.github.com>
2023-05-05 16:26:20 -0700

    fixed some shit

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const profileRoutes = require('./profileRoutes.js');
+const homeRoutes = require('./homeRoutes.js');
 
 router.use('/api', apiRoutes);
 router.use('/profile', profileRoutes);
+router.use('/', homeRoutes);
 
 module.exports = router;
